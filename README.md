82976890ed38ced73474eddfcb8dc442f9504845
Lan Nguyen <nguyenlan13@gmail.com>
2019-02-16 11:42:21 +0000

    Automatically backed up by Learn

--- README.md
+++ README.md
@@ -186,10 +186,10 @@ below:
 
 ```javascript
 var i = 0;
-
 function incrementVariable() {
   i = i + 1;
   return i;
+
 }
 
 do {
---
+++ loops.js
@@ -0,0 +1,48 @@
+function forLoop(array){
+
+for (let i = 0; i < 25; i++){
+
+  if (i===1) { 
+   array.push ("I am 1 strange loop.");
+  }
+
+ else{
+  array.push ("I am " + i+ " strange loops.");
+ }
+}
+return array;
+}
+
+
+
+function whileLoop(n){
+
+  while ( n> 0){
+  
+    console.log (--n);
+  
+}
+return "done";
+}
+
+
+
+function doWhileLoop (i){
+ 
+  
+  function incrementVariable() {
+  i = i + 1;
+  return i;
+
+}
+
+
+
+do { 
+  console.log(i + 10);
+} while (incrementVariable() < 10);
+  
+  return 'I run once regardless.';
+  
+}
+  
